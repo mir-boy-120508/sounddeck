89da417415f61af669dad78143b488d99fcbc0de
mir-boy-120508 <mir.boy.20120508@gmail.com>
2026-06-05 05:16:20 +0900

    Add files via upload

---
+++ lyrics_database.js.js
@@ -0,0 +1,19 @@
+// lyrics_database.js
+window.SOUNDDECK_LYRICS = {
+  "SN-938210": [
+    { time: 0, text: "いきものがかり - 気まぐれロマンティック" },
+    { time: 3, text: "ロマンティック 恋のアンテナは" }
+  ],
+  "SN-105724": [
+    { time: 0, text: "HY - AM11:00" },
+    { time: 31.43, text: "「目を覚ましてよ」君の声が" }
+  ],
+  "SN-482015": [ { time: 0, text: "JUDY AND MARY - 自転車" } ],
+  "SN-773912": [ { time: 0, text: "チャットモンチー - 風吹けば恋" } ],
+  "SN-228104": [ { time: 0, text: "GReeeeN - 愛唄" } ],
+  "SN-610493": [ { time: 0, text: "Every Little Thing - 出逢った頃のように" } ],
+  "SN-335281": [ { time: 0, text: "中島美嘉 - STARS" } ],
+  "SN-559201": [ { time: 0, text: "大原ゆい子 - 言わないけどね" } ],
+  "SN-882140": [ { time: 8, text: "あたしのやさしい気持ちを 分け合いたいから" } ],
+  "SN-114958": [ { time: 0, text: "大原ゆい子 - 君と光" } ]
+};
---
+++ song_titles.js.js
@@ -0,0 +1,13 @@
+// song_titles.js
+window.SOUNDDECK_SONG_TITLES = {
+  "SN-938210": { id: 0, title: "気まぐれロマンティック", artist: "いきものがかり", src: "song1.mp3", cover: "jacket1.png" },
+  "SN-105724": { id: 1, title: "AM11:00", artist: "HY", src: "song2.mp3", cover: "jacket2.png" },
+  "SN-482015": { id: 2, title: "自転車", artist: "JUDY AND MARY", src: "song3.mp3", cover: "jacket3.png" },
+  "SN-773912": { id: 3, title: "風吹けば恋", artist: "チャットモンチー", src: "song4.mp3", cover: "jacket4.png" },
+  "SN-228104": { id: 4, title: "愛唄", artist: "GReeeeN", src: "song5.mp3", cover: "jacket5.png" },
+  "SN-610493": { id: 5, title: "出逢った頃のように", artist: "Every Little Thing", src: "song6.mp3", cover: "jacket6.png" },
+  "SN-335281": { id: 6, title: "STARS", artist: "中島美嘉", src: "song7.mp3", cover: "jacket7.png" },
+  "SN-559201": { id: 7, title: "言わないけどね", artist: "大原ゆい子", src: "song8.mp3", cover: "jacket8.png" },
+  "SN-882140": { id: 8, title: "やさしい気持ち", artist: "Chara", src: "song9.mp3", cover: "jacket9.png" },
+  "SN-114958": { id: 9, title: "君と光", artist: "大原ゆい子", src: "song10.mp3", cover: "jacket10.png" }
+};
